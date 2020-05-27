1a689a48faff632379b7fdc0d769dcad59ecc8cc
E Eue <elena-urra@hotmail.com>
2020-05-27 18:15:44 +0200

    cambio en el método OnPost()

--- ClubPadel/Pages/Index.cshtml.cs
+++ ClubPadel/Pages/Index.cshtml.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using ClubPadel.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 
@@ -34,27 +35,59 @@ namespace ClubPadel.Pages
             Clientes = await _db.Cliente.ToListAsync();
         }
 
-        //si se añade el metodo onPost() es para añadir los datos a la base de datos        
-        public async Task<IActionResult> OnPost(String nombreUsuario,String contraseñaUsuario)
-        {
-            string nombreUsu = nombreUsuario;
-            string contraseñaUsu = contraseñaUsuario;
-            if (!ModelState.IsValid)
-            {
-                for (int i = 1; i <= 5; i++)//no me reconoce Clientes 
-                {//cambiar el i<= 5 por los numero id maximo de la base de datos
+        //si se añade el metodo onPost() es para añadir los datos a la base de datos      
+        //Opción con for
 
-                    var clientito = await _db.Cliente.FindAsync(i);
-                    if (clientito != null)
+        /* public async Task<IActionResult> OnPost(String nombreUsuario,String contraseñaUsuario)
+         {
+              string nombreUsu = nombreUsuario;
+              string contraseñaUsu = contraseñaUsuario;
+              if (!ModelState.IsValid)
+              {
+                  for (int i = 1; i <= 5; i++)//no me reconoce Clientes 
+                  {//cambiar el i<= 5 por los numero id maximo de la base de datos
+
+                      var clientito = await _db.Cliente.FindAsync(i);
+                      if (clientito != null)
+                      {
+                          if (clientito.user == nombreUsu && clientito.password==contraseñaUsu)
+                          {
+                              return RedirectToPage("ReservasPista");
+                          }
+                      }
+                  }
+              }
+              return Page();    
+         }*/
+
+        public async Task<IActionResult> OnPost(String nombreUsuario, String contraseñaUsuario)
+        {
+            try
+            {
+                var cb = new SqlConnectionStringBuilder();
+
+                cb.DataSource = "Server=(localdb)\\mssqllocaldb;Database=ClubPadel;Trusted_Connection=True;MultipleActiveResultSets=true";
+                using (var connection = new SqlConnection(cb.ConnectionString))
+                {
+                    connection.Open();
+                    foreach (Cliente item in Clientes)
                     {
-                        if (clientito.user == nombreUsu && clientito.password==contraseñaUsu)
+                        if (item != null)
                         {
-                            return RedirectToPage("ReservasPista");
+                            if (item.user == nombreUsuario && item.password == contraseñaUsuario)
+                            {
+                                return RedirectToPage("ReservasPistas");
+                            }
                         }
                     }
                 }
             }
-            return Page();          
+            catch (SqlException e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+
+            return Page();
         }
     }
 }
