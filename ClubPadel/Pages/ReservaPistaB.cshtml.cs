779949909145d9fd814d3f4071ffd6c3aeb06b22
Erick Chasco <pantuflin4@gmail.com>
2020-06-02 13:11:05 +0200

    Retoques

--- ClubPadel/Pages/ReservaPistaB.cshtml.cs
+++ ClubPadel/Pages/ReservaPistaB.cshtml.cs
@@ -2,15 +2,47 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ClubPadel.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 
 namespace ClubPadel.Pages
 {
     public class ReservaPistaBModel : PageModel
     {
-        public void OnGet()
+        private readonly ApplicationDbContext _db;
+
+        //-----------------------------------
+
+        [BindProperty]
+        public TablaMañana TablaMañana { get; set; }
+
+        private readonly ILogger<ReservasPistaModel> _logger;
+
+        public ReservaPistaBModel(ILogger<ReservasPistaModel> logger, ApplicationDbContext db)
         {
+            _logger = logger;
+            _db = db;
+        }
+        public IEnumerable<TablaMañana> TablaMañanas { get; set; }
+
+        public async Task OnGet()
+        {
+            TablaMañanas = await _db.TablaMañana.ToListAsync();
+        }
+
+        //si se añade el metodo onPost() es para añadir los datos a la base de datos        
+        public async Task<IActionResult> OnPost(string btnreserva)
+        {
+            //String valorreserva = btnreserva;
+
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            return RedirectToPage("ReservaPistaB");
 
         }
     }
