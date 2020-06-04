3184c57d21ef33de68b1a658538f73bf06e1dbdc
E Eue <elena-urra@hotmail.com>
2020-06-04 13:12:45 +0200

    actualizo

--- ClubPadel/Pages/ReservasPistaA.cshtml.cs
+++ ClubPadel/Pages/ReservasPistaA.cshtml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Threading.Tasks;
 using ClubPadel.Models;
 using Microsoft.AspNetCore.Mvc;
@@ -30,25 +31,53 @@ namespace ClubPadel.Pages
         }
         public IEnumerable<TablaHoy> TablaHoys { get; set; }
 
-        public IEnumerable<TablaPrueba> TablaPruebas { get; set; }
+        public IEnumerable<TablaPrueba> TablaPruebas { get; set; }//no es lo que queremos nos da null por alguna razón 
 
         public async Task OnGet()
         {
-            TablaHoys = await _db.TablaHoy.ToListAsync();
             TablaPruebas = await _db.TablaPrueba.ToListAsync();
+            TablaHoys = await _db.TablaHoy.ToListAsync();
+           
         }
 
         //si se añade el metodo onPost() es para añadir los datos a la base de datos        
-        public async Task<IActionResult> OnPost(string btnreserva)
+        public async Task<IActionResult> OnPost()
         {
-            //String valorreserva = btnreserva;
-
             if (!ModelState.IsValid)
             {
                 return Page();
             }
-            return RedirectToPage("ReservaPistaB");
-           
+                return RedirectToPage("ReservaPistaB");
         }
-    }
+
+
+
+        public async Task<IActionResult> OnPostCambio(string estadito)
+        {
+
+            var cb = new SqlConnectionStringBuilder();
+            var Pruebita = _db.TablaPrueba;
+
+            cb.DataSource = "localhost\\CLUBPADEL";
+            using (var connection = new SqlConnection(cb.ConnectionString))
+            {
+                foreach (var item in Pruebita)
+                {
+                    if (item.Estado == estadito)
+                    {                       
+                        item.Estado = "Reservado";
+                        _db.Entry(item.Estado).State = EntityState.Modified;//nose pa que sirve
+                    }
+                    if (item.Estado == "Reservado")
+                    {
+                        item.Estado = "Libre";
+                    }
+                }
+            }
+            await _db.SaveChangesAsync();//update
+
+            return Page();
+        }
+
+        }
 }
